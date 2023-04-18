1ecd3c4c714b0ba1e2ca21e84f51a43a0a963b57
Den <52295780+josserden@users.noreply.github.com>
2023-04-18 17:39:52 +0300

    create base structure for users page

--- src/page-components/UsersPage/UsersPage.jsx
+++ src/page-components/UsersPage/UsersPage.jsx
@@ -1,10 +1,26 @@
+import Grid from '@mui/material/Unstable_Grid2';
+
+import { ContactCard } from 'components/ContactCard';
 import { PageWrapper } from 'components/PageWrapper';
-import React from 'react';
 
 export const UsersPage = () => {
   return (
     <PageWrapper>
       <h1>Users Page</h1>
+
+      <Grid container spacing={3}>
+        <Grid xs={4}>
+          <ContactCard />
+        </Grid>
+
+        <Grid xs={4}>
+          <ContactCard />
+        </Grid>
+
+        <Grid xs={4}>
+          <ContactCard />
+        </Grid>
+      </Grid>
     </PageWrapper>
   );
 };
